818e5d0a64fc3a78299617305757571392204f05
Matthew <99067338@mydavinci.nl>
2021-09-09 11:18:44 +0200

    added person.py with screens of the code / final product

---
+++ person.py
@@ -0,0 +1,13 @@
+print ('Gegevens Invoer')
+name = input ('geef jouw naam: ')
+address = input ('geef jouw adderes: ')
+zipcode = input ('geef jouw postcode: ')
+residence = input ('geef jouw woonplaats: ')
+
+print ('----------------------------------------------------')
+print ('Naam:' + name)
+print ('Adderes:' + address)
+print ('Postcode:' + zipcode)
+print ('Woonplaats:' + residence)
+print ('----------------------------------------------------')
+ 
